b863d3ebaeaa2755be6e398190af5051d7727f31
Daniel Terranova <daniel.terranova@gmail.com>
2017-10-05 07:48:41 +0200

    Max

--- src/com/terraware/Sort.java
+++ src/com/terraware/Sort.java
@@ -9,21 +9,17 @@ public class Sort {
     public Sort() {}
 
     public static int[] doInsertionSort(int[] input) {
-
-        int temp;
         for (int i = 1; i < input.length; i++) {
             for (int j = i ; j > 0 ; j--) {
                 if (input[j] < input[j-1]) {
-                    temp = input[j];
-                    input[j] = input[j-1];
-                    input[j-1] = temp;
+                    iswap(j,j-1,input);
                 }
             }
         }
         return input;
     }
 
-    public static int[] doBubbleSort(int[] arr) {
+    static int[] doBubbleSort(int[] arr) {
         int n = arr.length;
         int k;
         for (int m = n; m >= 0; m--) {
@@ -33,9 +29,26 @@ public class Sort {
                     iswap(i, k, arr);
                 }
             }
-
         }
         return arr;
+    }
+
+    private static int max(int[] arr, int offset, int currentMax) {
+        if (offset == arr.length - 1)
+            return currentMax;
+        else {
+            int max;
+            if (currentMax < arr[offset]) {
+                max = arr[offset];
+            } else {
+                max = currentMax;
+            }
+            return max(arr, ++offset, max);
+        }
+    }
+
+    static int max(int[] arr) {
+        return max(arr, 0, Integer.MIN_VALUE);
     }
 
     private static void iswap(int i, int j, int[] a) {
--- test/src/com/terraware/SimpleTest.java
+++ test/src/com/terraware/SimpleTest.java
@@ -2,7 +2,12 @@ package com.terraware;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.NavigableSet;
+import java.util.TreeSet;
 
 /**
  * @author Daniel Terranova <mailto:daniel.terranova@so4it.com>
@@ -30,5 +35,36 @@ class SimpleTest {
         System.out.println(Arrays.toString(Sort.doBubbleSort(input)));
     }
 
+    @Test
+    public void testInsertSort() {
+        int[] input = { 4, 2, 9, 6, 23, 12, 34, 0, 1 };
 
+        System.out.println(Arrays.toString(Sort.doInsertionSort(input)));
+    }
+
+    @Test
+    public void testRecursiveMax() {
+        int[] input = { 4, 2, 9, 6, 23, 12, 34, 35, 1 };
+
+        System.out.println("Max=" + Sort.max(input));
+    }
+
+    @Test
+    void testNavigableSet() {
+        LocalDate _2017_12_1 = LocalDate.of(2017, 12, 1);
+        LocalDate _2018_12_2 = LocalDate.of(2018, 12, 2);
+        LocalDate _2019_12_3 = LocalDate.of(2019, 12, 3);
+        LocalDate _2020_12_3 = LocalDate.of(2020, 12, 3);
+        NavigableSet<Date> original = new TreeSet<>();
+        original.add(toDate(_2017_12_1));
+        //original.add(toDate(_2018_12_2));
+        original.add(toDate(_2019_12_3));
+        original.add(toDate(_2020_12_3));
+        System.out.println("original = " + original.floor(toDate(LocalDate.of(2018, 12, 2))));
+
+    }
+
+    Date toDate(LocalDate localDate) {
+        return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    }
 }
